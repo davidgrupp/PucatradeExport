3dae18fdce3834f27869aa748ac7c801c5bf2056
david grupp <david@grupp.me>
2015-09-03 22:52:07 -0500

    added one line version

---
+++ PucatradeExportOneLine.js
@@ -0,0 +1,1 @@
+var addresses = ""; $('.copy_to_clipboard').each(function(i,v) { addresses += '"'+ $(v).attr('onclick').replace('window.prompt("Copy to clipboard: Ctrl+C, Enter", "','').replace('");','').replace(/\\n/g, '\r\n') +'"\r\n'; }); console.log(addresses);
