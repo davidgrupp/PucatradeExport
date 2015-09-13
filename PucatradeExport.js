d1bb273fbce9fe0fa81a3c46dcccde307b86f59a
david grupp <david@grupp.me>
2015-09-12 20:05:34 -0500

    updated export to export card name and trade number

--- PucatradeExport.js
+++ PucatradeExport.js
@@ -1,9 +1,13 @@
 var addresses = "";
-$('.copy_to_clipboard').each(function(i,v) { 
-	addresses += '"'+ $(v).attr('onclick')
-						.replace('window.prompt("Copy to clipboard: Ctrl+C, Enter", "','')
-						.replace('");','')
-						.replace(/\\n/g, '\r\n')
-						+'"\r\n'; 
+$('td:contains("Shipped Card")').each(function(i,v) { 
+	var row = $(v).parent();
+	var number = row.find('td:nth-child(1)').text().replace(/\s/g, '');
+	var address =  row.find('.copy_to_clipboard')
+					.attr('onclick')
+					.replace('window.prompt("Copy to clipboard: Ctrl+C, Enter", "','')
+					.replace('");','')
+					.replace(/\\n/g, '\r\n');
+	var card = row.find('td:nth-child(3) a.cl').text();
+	addresses += (number + ',' + card + ',"' + address + '"\r\n');
 });
 console.log(addresses);
